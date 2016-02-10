6a3f0e9c35cac4434dc8cff1117d57985876caa9
Darren Guy <thesoftwaredude@outlook.com>
2016-02-10 21:45:31 +0000

    Base solution

---
+++ InOut.UI/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace InOut.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ InOut.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="InOut.UI.MvcApplication" Language="C#" %>
